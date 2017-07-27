a4a1f415ee791096cf98ff79576d376a0aabcc47
Alexandre Quessy <alexandre@quessy.net>
2017-07-26 23:12:01 -0400

    more identation fixes

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = config.port;    // set our port
-var mongoose   = require('mongoose');
+var mongoose = require('mongoose');
 mongoose.connect(config.database); // database
 app.set('superSecret', config.secret); // secret variable
 var db = mongoose.connection;
@@ -31,7 +31,7 @@ db.once('open', function callback () {
 
 //ROUTES FOR OUR API
 //=============================================================================
-var router = express.Router();              // get an instance of the express Router
+var router = express.Router(); // get an instance of the express Router
 
 // Middleware before authentification
 router.post('/setup', function(req, res) {
@@ -93,65 +93,59 @@ router.post('/authenticate', function(req, res) {
 
 //middleware to use for all requests while authenticated
 router.use(function(req, res, next) {
-
-      // check header or url parameters or post parameters for token
-      var token = req.body.token || req.query.token || req.headers['x-access-token'];
-
-      // decode token
-      if (token) {
-
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+    // decode token
+    if (token) {
         // verifies secret and checks exp
         jwt.verify(token, app.get('superSecret'), function(err, decoded) {      
-          if (err) {
-            return res.json({ success: false, message: 'Failed to authenticate token.' });    
-          } else {
-            // if everything is good, save to request for use in other routes
-            req.decoded = decoded;    
-            next();
-          }
+            if (err) {
+                return res.json({
+                    success: false, message: 'Failed to authenticate token.'
+                });    
+            } else {
+                // if everything is good, save to request for use in other routes
+                req.decoded = decoded;    
+                next();
+            }
         });
-
-      } else {
-
+    } else {
         // if there is no token
         // return an error
         return res.status(403).send({ 
             success: false, 
             message: 'No token provided.' 
         });
-        
-      }
-    });
+    }
+});
 
 //test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
- res.json({ message: 'hooray! welcome to our api!' });   
+    res.json({ message: 'hooray! welcome to our api!' });   
 });
 
-
-router.route('/users') 
 //get all the users (accessed at GET http://localhost:8080/api/users)
-    .get(function(req, res) {
-        User.find(function(err, users) {
-            if (err)
-                res.send(err);
-                res.json(users);
- });
+router.route('/users').get(function(req, res) {
+    User.find(function(err, users) {
+        if (err) {
+            res.send(err);
+        }
+        res.json(users);
+    });
 });
-
-router.route('/users/:users_id')
 
 // get the user with that id (accessed at GET http://localhost:8080/api/bears/:users_id)
-.get(function(req, res) {
+router.route('/users/:users_id').get(function(req, res) {
     User.findById(req.params.users_id, function(err, user) {
-        if (err)
+        if (err) {
             res.send(err);
+        }
         res.json(user);
     });
 });
 
+// use our user model to find the user we want
 router.route('/users/:users_id').put(function(req, res) {
-    // use our user model to find the user we want
     User.findById(req.params.users_id, function(err, user) {
         if (err) {
             res.send(err);
@@ -190,5 +184,5 @@ app.use('/api', router);
 // START THE SERVER
 // =============================================================================
 app.listen(port);
-console.log('Magic happens on port ' + port);
+console.log('Listening on port ' + port);
 
