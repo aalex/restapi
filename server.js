423582799b41ef6b4b168359e2e4b16f5fead72a
Alexandre Quessy <alexandre@quessy.net>
2017-07-26 23:17:40 -0400

    more indentation fixes

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-// server.js
-
-// BASE SETUP
-// =============================================================================
+/**
+ * This files is our main entry for our web server application.
+ * 
+ * port: 8080
+ * Routes:
+ * - POST /setup - Middleware before authentification
+ * - POST /authenticate - Check password and username is found. Provide token.
+ */
 
 // call the packages we need
 var express = require('express');        // call express
@@ -23,9 +27,9 @@ var mongoose = require('mongoose');
 mongoose.connect(config.database); // database
 app.set('superSecret', config.secret); // secret variable
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on('error', console.error.bind(console, 'Database connection error:'));
 db.once('open', function callback () {
-    console.log("db started ------------");
+    console.log("Opened database");
 });
 
 
@@ -55,43 +59,38 @@ router.post('/setup', function(req, res) {
 });
 
 router.post('/authenticate', function(req, res) {
-
-      // find the user
-      User.findOne({
+    // find the user
+    User.findOne({
         name: req.body.name
-      }, function(err, user) {
-
-        if (err) throw err;
-
-        if (!user) {
-          res.json({ success: false, message: 'Authentication failed. User not found.' });
-        } else if (user) {
-
-          // check if password matches
-          if (user.password != req.body.password) {
-            res.json({ success: false, message: 'Authentication failed. Wrong password.' });
-          } else {
-
-            // if user is found and password is right
-            // create a token
-            var token = jwt.sign(user, app.get('superSecret'), {
-              expiresInMinutes: 1440 // expires in 24 hours
-            });
-
-            // return the information including token as JSON
-            res.json({
-              success: true,
-              message: 'Enjoy your token!',
-              token: token
-            });
-          }   
-
+    }, function(err, user) {
+        if (err) {
+            throw err;
         }
+        if (! user) {
+            res.json({ success: false, message: 'Authentication failed. User not found.' });
+        } else if (user) {
+            // check if password matches
+            if (user.password != req.body.password) {
+                res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+            } else {
+                // if user is found and password is right
+                // create a token
+                var token = jwt.sign(user, app.get('superSecret'), {
+                    expiresInMinutes: 1440 // expires in 24 hours
+                });
 
-      });
+                // return the information including token as JSON
+                res.json({
+                    success: true,
+                    message: 'Enjoy your token!',
+                    token: token
+                });
+            }
+        }
     });
+});
 
-//middleware to use for all requests while authenticated
+// middleware to use for all requests while authenticated
 router.use(function(req, res, next) {
     // check header or url parameters or post parameters for token
     var token = req.body.token || req.query.token || req.headers['x-access-token'];
