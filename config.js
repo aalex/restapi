6cba0cfc86d78925f626d470cfe24cee9a4f1e58
Alexandre Quessy <alexandre@quessy.net>
2017-07-26 22:46:21 -0400

    chmod -x

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ module.exports = {
     'secret': '8jh%sdE#0',
     'database': 'mongodb://localhost/test',
     'host': 'localhost',
-    'port': 8080
-
+    'port': 8080,
+    'license': 'Apache license 2.0',
+    'repository': 'https://github.com/aalex/consent'
 };
